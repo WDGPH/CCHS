3606de6efc2458b8be304ebff6e0595de9dbd09c
Eswar Attuluri <eswar.attuluri@wdgpublichealth.ca>
2025-11-27 13:43:20 -0500

    : add Python version 3.12 to project configuration and added aged groups analysis

--- config/settings.py
+++ config/settings.py
@@ -51,6 +51,36 @@ CONFIDENCE_LEVEL = 0.95
 BOOTSTRAP_PREFIX = "BSW"
 
 # Age group settings
-AGE_BINS = [0, 18, 25, 45, 65, 120]
-AGE_LABELS = ['0-17', '18-24', '25-44', '45-64', '65+']
+# Default age group configurations (can be customized by user)
+DEFAULT_AGE_BINS = [0, 15, 25, 45, 65, 120]
+DEFAULT_AGE_LABELS = ['0-14', '15-24', '25-44', '45-64', '65+']
+
+# Preset age group options
+AGE_GROUP_PRESETS = {
+    "Standard (5 groups)": {
+        "bins": [0, 15, 25, 45, 65, 120],
+        "labels": ['0-14', '15-24', '25-44', '45-64', '65+']
+    },
+    "Detailed (7 groups)": {
+        "bins": [0, 18, 25, 35, 45, 55, 65, 120],
+        "labels": ['0-17', '18-24', '25-34', '35-44', '45-54', '55-64', '65+']
+    },
+    "Youth Focus (6 groups)": {
+        "bins": [0, 12, 18, 25, 35, 65, 120],
+        "labels": ['0-11', '12-17', '18-24', '25-34', '35-64', '65+']
+    },
+    "Senior Focus (6 groups)": {
+        "bins": [0, 45, 55, 65, 75, 85, 120],
+        "labels": ['0-44', '45-54', '55-64', '65-74', '75-84', '85+']
+    },
+    "Simple (3 groups)": {
+        "bins": [0, 18, 65, 120],
+        "labels": ['0-17', '18-64', '65+']
+    },
+    "Custom": {
+        "bins": None,  # User will define
+        "labels": None
+    }
+}
+
 AGE_COLUMN = 'DHH_AGE'
